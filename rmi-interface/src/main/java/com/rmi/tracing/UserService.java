0458ff5908ff13d9d19ddb918068906775720eb5
Ryan Skinner <rskinner593@gmail.com>
2025-09-18 06:42:32 -0600

    Created two new interfaces getUser and getAllUser

--- rmi-interface/src/main/java/com/rmi/tracing/UserService.java
+++ rmi-interface/src/main/java/com/rmi/tracing/UserService.java
@@ -5,4 +5,6 @@ import java.rmi.RemoteException;
 
 public interface UserService extends Remote {
     void updateUser(String userId, String userData) throws RemoteException;
+    String getUser(String userId) throws RemoteException;
+    String getAllUsers() throws RemoteException;
 }
